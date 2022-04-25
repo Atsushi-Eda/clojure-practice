784775be86b398243cff7dd9eca84d81c7e2f907
Atsushi-Eda <aeda0922@gmail.com>
2022-04-25 21:55:48 +0900

    Add todo app

--- web/src/main/web/core.cljs
+++ web/src/main/web/core.cljs
@@ -1,4 +1,16 @@
-(ns web.core)
+(ns web.core
+      (:require
+            [reagent.dom :as reagent]))
+
+(def ^:private todos [{:id 1 :title "牛乳を買う"} {:id 2 :title "牛乳を飲む"}])
+
+(defn app []
+      [:div
+       (map (fn [todo] [:p {:key (todo :id)} (todo :title)]) todos)])
+
+(defn ^:dev/after-load render
+      []
+      (reagent/render [app] (.getElementById js/document "app")))
 
 (defn ^:export init []
-      (js/console.log "hello world"))
+      (render))
