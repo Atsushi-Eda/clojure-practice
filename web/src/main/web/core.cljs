20e6bb839df60eed4d93a9c03a9594b70870d013
Atsushi-Eda <aeda0922@gmail.com>
2022-04-28 00:03:35 +0900

    Use react-query

--- web/src/main/web/core.cljs
+++ web/src/main/web/core.cljs
@@ -1,6 +1,7 @@
 (ns web.core
       (:require
-            [reagent.dom :as reagent]))
+            [reagent.dom :as reagent]
+            [react-query :as rq :refer (QueryClientProvider QueryClient)]))
 
 (def ^:private todos [{:id 1 :title "牛乳を買う"} {:id 2 :title "牛乳を飲む"}])
 
