2f23166c96e7b93298873dd13aff3f2ac83db2a9
Atsushi-Eda <aeda0922@gmail.com>
2022-05-07 17:07:43 +0900

    Format

--- api/src/api/core.clj
+++ api/src/api/core.clj
@@ -1,10 +1,11 @@
 (ns api.core
-    (:gen-class)
-    (:require [bidi.ring :as ring]
-      [ring.middleware.json :as ring-json]
-      [clojure.data.json :as json]
-      [ring.adapter.jetty :as jetty]
-      [clojure.java.jdbc :as jdbc]))
+  (:gen-class)
+  (:require
+   [bidi.ring :as ring]
+   [clojure.data.json :as json]
+   [clojure.java.jdbc :as jdbc]
+   [ring.adapter.jetty :as jetty]
+   [ring.middleware.json :as ring-json]))
 
 (def mysql-db {:dbtype "mysql"
                :host "localhost"
@@ -13,45 +14,41 @@
                :user "root"
                :password "clojure-practice"})
 
-(defn select-todos[]
-      (jdbc/query mysql-db
-                  ["select * from todo"]))
+(defn select-todos []
+  (jdbc/query mysql-db
+              ["select * from todo"]))
 
-(defn insert-todo[title]
-      (jdbc/insert! mysql-db
-                  :todo {:title title}))
+(defn insert-todo [title]
+  (jdbc/insert! mysql-db
+                :todo {:title title}))
 
 (defn- get-todos-handler [request]
-       {:status 200
-        :headers {
-                  "Content-Type" "application/json; charset=utf-8";; TODO: middlewareで設定する
-                  "Access-Control-Allow-Origin" "http://localhost:8080" ;; TODO: middlewareで設定する
-                  }
-        :body (json/write-str (select-todos))})
+  {:status 200
+   :headers {"Content-Type" "application/json; charset=utf-8";; TODO: middlewareで設定する
+             "Access-Control-Allow-Origin" "http://localhost:8080" ;; TODO: middlewareで設定する
+             }
+   :body (json/write-str (select-todos))})
 
 (defn- post-todo-handler [request]
-       (let [id ((first (insert-todo ((request :body) :title))) :generated_key)]
-       {:status 200
-        :headers {
-                  "Content-Type" "application/json; charset=utf-8";; TODO: middlewareで設定する
-                  "Access-Control-Allow-Origin" "http://localhost:8080" ;; TODO: middlewareで設定する
-                  }
-        :body (json/write-str {:id id})}))
+  (let [id ((first (insert-todo ((request :body) :title))) :generated_key)]
+    {:status 200
+     :headers {"Content-Type" "application/json; charset=utf-8";; TODO: middlewareで設定する
+               "Access-Control-Allow-Origin" "http://localhost:8080" ;; TODO: middlewareで設定する
+               }
+     :body (json/write-str {:id id})}))
 
 (defn- options-todo-handler [request]
-       {:status 200
-        :headers {
-                  "Access-Control-Allow-Origin" "http://localhost:8080"
-                  "Access-Control-Allow-Headers" "Content-Type" ;; TODO: 各handlerのAccess-Control-Allow-Originと合わせてRing CORSに移行
-                  }})
+  {:status 200
+   :headers {"Access-Control-Allow-Origin" "http://localhost:8080"
+             "Access-Control-Allow-Headers" "Content-Type" ;; TODO: 各handlerのAccess-Control-Allow-Originと合わせてRing CORSに移行
+             }})
 
 (defn- not-found-handler [request]
-       {:status 404
-        :headers {
-                  "Content-Type" "application/json; charset=utf-8" ;; TODO: middlewareで設定する
-                  "Access-Control-Allow-Origin" "http://localhost:8080" ;; TODO: middlewareで設定する
-                  }
-        :body (json/write-str {:message "Not Found"})})
+  {:status 404
+   :headers {"Content-Type" "application/json; charset=utf-8" ;; TODO: middlewareで設定する
+             "Access-Control-Allow-Origin" "http://localhost:8080" ;; TODO: middlewareで設定する
+             }
+   :body (json/write-str {:message "Not Found"})})
 
 (def ^:private route
   ["/" {"todo" {:get get-todos-handler :post post-todo-handler :options options-todo-handler}
@@ -62,7 +59,7 @@
       (ring-json/wrap-json-body {:key-fn keyword})))
 
 (defn -main
-      [& args]
-      (jetty/run-jetty handler {:host "localhost"
-                                :port 8081
-                                :join? false}))
+  [& args]
+  (jetty/run-jetty handler {:host "localhost"
+                            :port 8081
+                            :join? false}))
--- web/src/main/web/core.cljs
+++ web/src/main/web/core.cljs
@@ -1,34 +1,34 @@
 (ns web.core
-      (:require
-            [reagent.dom :as reagent]
-            [react-query :as rq :refer (QueryClientProvider QueryClient)]))
+  (:require
+   [react-query :as rq :refer (QueryClientProvider QueryClient)]
+   [reagent.dom :as reagent]))
 
 (def queryClient (QueryClient.))
 
 (defn useTodos []
-      (rq/useQuery "todos" (fn [] (-> (js/fetch "http://localhost:8081/todo")
-                               (.then (fn [response] (.json response)))))))
+  (rq/useQuery "todos" (fn [] (-> (js/fetch "http://localhost:8081/todo")
+                                  (.then (fn [response] (.json response)))))))
 
 (defn usePostTodo []
-      (rq/useMutation (fn [title] (js/fetch "http://localhost:8081/todo" (clj->js {:method "POST" :headers {:Content-Type "application/json"} :body (js/JSON.stringify (clj->js {:title title}))})))
-                      #js {:onSuccess (fn [response title] (-> (.json response) (.then (fn [json] (.setQueryData queryClient "todos" (fn [old] (clj->js (conj (js->clj old) {:id (.-id json) :title title}))))))))}))
+  (rq/useMutation (fn [title] (js/fetch "http://localhost:8081/todo" (clj->js {:method "POST" :headers {:Content-Type "application/json"} :body (js/JSON.stringify (clj->js {:title title}))})))
+                  #js {:onSuccess (fn [response title] (-> (.json response) (.then (fn [json] (.setQueryData queryClient "todos" (fn [old] (clj->js (conj (js->clj old) {:id (.-id json) :title title}))))))))}))
 
 (defn todo-list []
-        (let [response (useTodos)]
-             [:div (map (fn [todo] [:p {:key (todo :id)} (todo :title)]) ((js->clj response :keywordize-keys true) :data))]))
+  (let [response (useTodos)]
+    [:div (map (fn [todo] [:p {:key (todo :id)} (todo :title)]) ((js->clj response :keywordize-keys true) :data))]))
 
 (defn todo-form []
-      (let [mutation (usePostTodo)]
-           [:input {:type "text" :onKeyDown (fn [event] (if (= "Enter" (.-code event)) (mutation.mutate (.-value (.-target event)))))}]))
+  (let [mutation (usePostTodo)]
+    [:input {:type "text" :onKeyDown (fn [event] (if (= "Enter" (.-code event)) (mutation.mutate (.-value (.-target event)))))}]))
 
 (defn app []
-      [:> QueryClientProvider {:client queryClient}
-          [:f> todo-list]
-          [:f> todo-form]])
+  [:> QueryClientProvider {:client queryClient}
+   [:f> todo-list]
+   [:f> todo-form]])
 
 (defn ^:dev/after-load render
-      []
-      (reagent/render [app] (.getElementById js/document "app")))
+  []
+  (reagent/render [app] (.getElementById js/document "app")))
 
 (defn ^:export init []
-      (render))
+  (render))
