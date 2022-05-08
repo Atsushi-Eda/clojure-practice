bbcf1dfa21899db0b5a6d99bebacf38cbb87c563
Atsushi-Eda <aeda0922@gmail.com>
2022-05-08 21:27:06 +0900

    Add todo-checkbox

--- web/src/main/web/core.cljs
+++ web/src/main/web/core.cljs
@@ -13,16 +13,24 @@
   (rq/useMutation (fn [title] (js/fetch "http://localhost:8081/todo" (clj->js {:method "POST" :headers {:Content-Type "application/json"} :body (js/JSON.stringify (clj->js {:title title}))})))
                   #js {:onSuccess (fn [response title] (-> (.json response) (.then (fn [json] (.setQueryData queryClient "todos" (fn [old] (clj->js (conj (js->clj old) {:id (.-id json) :title title}))))))))}))
 
+(defn useUpdateTodoCompleted []
+  (rq/useMutation (fn [{:keys [:id :completed]}] (js/fetch (str "http://localhost:8081/todo/" id) (clj->js {:method "PUT" :headers {:Content-Type "application/json"} :body (js/JSON.stringify (clj->js {:completed completed}))})))
+                  #js {:onSuccess (fn [_ {:keys [:id :completed]}] (.setQueryData queryClient "todos" (fn [old] (clj->js (assoc-in (js->clj old :keywordize-keys true) [(.indexOf (clj->js (map (fn [todo] (todo :id)) (js->clj old :keywordize-keys true))) id) :completed] completed)))))}))
+
 (defn useDeleteTodo []
   (rq/useMutation (fn [id] (js/fetch (str "http://localhost:8081/todo/" id) (clj->js {:method "DELETE"})))
                   #js {:onSuccess (fn [_ id] (.setQueryData queryClient "todos" (fn [old] (clj->js (filter (fn [todo] (not= id (.-id todo))) old)))))}))
+
+(defn todo-checkbox [todo]
+  (let [mutation (useUpdateTodoCompleted)]
+    [:input {:type "checkbox" :checked (todo :completed) :onChange (fn [] (mutation.mutate {:id (todo :id) :completed (not (todo :completed))}))}]))
 
 (defn delete-todo-button [id]
   (let [mutation (useDeleteTodo)]
     [:button {:onClick (fn [] (mutation.mutate id))} "x"]))
 
 (defn todo-item [todo]
-  [:div [:span (todo :title)] [:f> delete-todo-button (todo :id)]])
+  [:div [:f> todo-checkbox todo] [:span (todo :title)] [:f> delete-todo-button (todo :id)]])
 
 (defn todo-list []
   (let [response (useTodos)]
