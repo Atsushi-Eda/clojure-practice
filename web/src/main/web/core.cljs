e8a24763cc03bd4d3b3d23544e24218c930a14d7
Atsushi-Eda <aeda0922@gmail.com>
2022-05-08 17:43:46 +0900

    Add delete-todo-button

--- web/src/main/web/core.cljs
+++ web/src/main/web/core.cljs
@@ -13,8 +13,16 @@
   (rq/useMutation (fn [title] (js/fetch "http://localhost:8081/todo" (clj->js {:method "POST" :headers {:Content-Type "application/json"} :body (js/JSON.stringify (clj->js {:title title}))})))
                   #js {:onSuccess (fn [response title] (-> (.json response) (.then (fn [json] (.setQueryData queryClient "todos" (fn [old] (clj->js (conj (js->clj old) {:id (.-id json) :title title}))))))))}))
 
+(defn useDeleteTodo []
+  (rq/useMutation (fn [id] (js/fetch (str "http://localhost:8081/todo/" id) (clj->js {:method "DELETE"})))
+                  #js {:onSuccess (fn [_ id] (.setQueryData queryClient "todos" (fn [old] (clj->js (filter (fn [todo] (not= id (.-id todo))) old)))))}))
+
+(defn delete-todo-button [id]
+  (let [mutation (useDeleteTodo)]
+    [:button {:onClick (fn [] (mutation.mutate id))} "x"]))
+
 (defn todo-item [todo]
-  [:p (todo :title)])
+  [:div [:span (todo :title)] [:f> delete-todo-button (todo :id)]])
 
 (defn todo-list []
   (let [response (useTodos)]
